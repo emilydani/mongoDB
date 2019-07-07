2b5f13e86df5973fd56b503899633704df3c4d7d
emilydani <38664438+emilydani@users.noreply.github.com>
2019-07-07 07:18:55 -0700

    Add files via upload

---
+++ TodoApi2/ToDoApi2/Controllers/ToDoItemsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi2.Interfaces;
+using ToDoApi2.Models;
+
+namespace ToDoApi2.Controllers
+{
+	[Route("api/[controller]")]
+	public class ToDoItemsController : Controller
+	{
+		private readonly IToDoRepository _toDoRepository;
+
+		public ToDoItemsController(IToDoRepository toDoRepository)
+		{
+			_toDoRepository = toDoRepository;
+		}
+
+		[HttpGet]
+		public IActionResult List()
+		{
+			return Ok(_toDoRepository.All);
+		}
+
+		[HttpPost]
+		public IActionResult Create([FromBody] ToDoItem item)
+		{
+			try
+			{
+				if (item == null || !ModelState.IsValid)
+				{
+					return BadRequest(ErrorCode.TodoItemNameAndNotesRequired.ToString());
+				}
+				bool itemExists = _toDoRepository.DoesItemExist(item.ID);
+				if (itemExists)
+				{
+					return StatusCode(StatusCodes.Status409Conflict, ErrorCode.TodoItemIDInUse.ToString());
+				}
+				_toDoRepository.Insert(item);
+			}
+			catch (Exception)
+			{
+				return BadRequest(ErrorCode.CouldNotCreateItem.ToString());
+			}
+			return Ok(item);
+		}
+		public enum ErrorCode
+		{
+			TodoItemNameAndNotesRequired,
+			TodoItemIDInUse,
+			RecordNotFound,
+			CouldNotCreateItem,
+			CouldNotUpdateItem,
+			CouldNotDeleteItem
+		}
+
+	}
+
+}
---
+++ TodoApi2/ToDoApi2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using ToDoApi2;
+
+namespace ToDoApi2
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+
+
+			var host = new WebHostBuilder()
+		.UseKestrel()
+		.UseUrls("http://*:5000")
+		.UseContentRoot(Directory.GetCurrentDirectory())
+		.UseIISIntegration()
+		.UseStartup<Startup>()
+		.Build();
+
+		}
+
+
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>();
+	}
+}
